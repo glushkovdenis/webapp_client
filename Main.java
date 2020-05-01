1aa9ac61e2516983667fdbfcd26babff8a6e30f9
Denis Glushkov <56252019+glushkovdenis@users.noreply.github.com>
2020-05-01 23:30:10 +0300

    Final commit

--- Main.java
+++ Main.java
@@ -2,11 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите адрес конфига: ");
-        String str = s.next();
-        String[] arg = new String[1];
-        arg[0] = str;
-        new Client(arg);
+        new Client(args);
     }
 }
